2dc8bf50a1267b7a4b32ed1e27cfecae0358cc90
ritesh2503 <ritesh@legistify.com>
2022-06-18 16:41:44 +0530

    cors fix attempt

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({credentials: true, origin: 'https://service1-frontend-dev.legistrak.com'}));
 
 // routers
 app.get('/',(req,res)=>{
